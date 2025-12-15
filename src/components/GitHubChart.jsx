a8c335fd7c9cf410cc485f5ca6e75ea6559e5cdb
sam-kash <kashyapsankalpa@gmail.com>
2025-12-15 20:58:33 +0530

    added

--- src/components/GitHubChart.jsx
+++ src/components/GitHubChart.jsx
@@ -3,10 +3,28 @@ export default function GitHubChart() {
     <div className="github-chart">
       <h2>GitHub Activity</h2>
 
-      <img
-        src="https://ghchart.rshah.org/sam-kash"
-        alt="GitHub contribution chart"
-      />
+      <div className="chart-wrapper">
+        <img
+          src="https://ghchart.rshah.org/39d353/sam-kash"
+          alt="GitHub contribution chart"
+        />
+
+        <div className="chart-footer">
+          <span>Contributions in the last year</span>
+
+          <div className="legend">
+            <span>Less</span>
+            <div className="boxes">
+              <span className="l0" />
+              <span className="l1" />
+              <span className="l2" />
+              <span className="l3" />
+              <span className="l4" />
+            </div>
+            <span>More</span>
+          </div>
+        </div>
+      </div>
 
       <p className="proof-text">Proof of work</p>
     </div>
